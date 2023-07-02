6b04bd697c4d333733f931cf752141a1e1b3c8bb
Abhinav Tyagi <abhinavtyagi262@gmail.com>
2023-07-02 15:09:48 +0530

    Singleton Design Pattern code committed

--- src/designPattern/singleton/MainClass.java
+++ src/designPattern/singleton/MainClass.java
@@ -3,10 +3,10 @@ package designPattern.singleton;
 public class MainClass {
 
 	public static void main(String[] args) {
-		
-		SingletonClass singletonClass1 = SingletonClass.getInstance();	//same static instance is used everywhere
+
+		SingletonClass singletonClass1 = SingletonClass.getInstance(); // same static instance is used everywhere
 		SingletonClass singletonClass2 = SingletonClass.getInstance();
-		
+
 		singletonClass1.singletonClassMethod();
 
 	}
--- src/designPattern/singleton/SingletonClass.java
+++ src/designPattern/singleton/SingletonClass.java
@@ -1,19 +1,19 @@
 package designPattern.singleton;
 
 public class SingletonClass {
-	
-	private SingletonClass() {										//1) Creating private constructor
-		
+
+	private SingletonClass() { // 1) Creating private constructor
+
 	}
-	
-	static SingletonClass singletonClass = new SingletonClass();	//2) Creating static instance of class
-	
-	public static SingletonClass getInstance() {					//3) Creating static method to return the static instance
+
+	static SingletonClass singletonClass = new SingletonClass(); // 2) Creating static instance of class
+
+	public static SingletonClass getInstance() { // 3) Creating static method to return the static instance
 		return singletonClass;
 	}
-	
+
 	public void singletonClassMethod() {
 		System.out.println("singletonClassMethod is executed");
-	}	
-	
+	}
+
 }
